f125c3f5821dbea4f66a2489c111f947916cfb98
dlacko81 <lacko.daniel81@gmail.com>
2025-04-16 16:48:41 +0200

    Create DataTable.js

---
+++ components/DataTable.js
@@ -0,0 +1,34 @@
+function DataTable({ data }) {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Client Name</th>
+          <th>Date</th>
+          <th>Volume</th>
+          <th>Vintage</th>
+          <th>Technology</th>
+          <th>Country</th>
+          <th>Price</th>
+          <th>Comments</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((row, index) => (
+          <tr key={index}>
+            <td>{row.clientName}</td>
+            <td>{row.date}</td>
+            <td>{row.volume}</td>
+            <td>{row.vintage}</td>
+            <td>{row.technology}</td>
+            <td>{row.country}</td>
+            <td>{row.price}</td>
+            <td>{row.comments}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+export default DataTable;
