664c1782b77a82b3248ebe3b9debe75e5daa239e
dlacko81 <lacko.daniel81@gmail.com>
2025-04-16 16:47:52 +0200

    Delete components/Stats.tsx

--- components/Stats.tsx
+++
@@ -1,7 +0,0 @@
-export default function Stats() {
-  return (
-    <div className="mb-6 p-4 bg-white rounded shadow">
-      <p className="text-lg font-medium">Summary stats will go here (aggregations, charts)</p>
-    </div>
-  );
-}
