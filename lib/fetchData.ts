2263f465da3f85ae26f527f629bcded635eca5f2
dlacko81 <lacko.daniel81@gmail.com>
2025-04-16 16:34:19 +0200

    Delete lib directory

--- lib/fetchData.ts
+++
@@ -1,5 +0,0 @@
-export async function fetchSheetData() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/data`);
-  if (!res.ok) throw new Error("Failed to fetch data");
-  return res.json();
-}
