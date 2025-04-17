6367451cf00e37dc4cf866c54c2a06313cd74dba
dlacko81 <lacko.daniel81@gmail.com>
2025-04-17 10:48:27 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 // pages/_app.js
 
-import '@/styles/globals.css' // make sure the path is correct
+import '../styles/globals.css' // make sure the path is correct
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
